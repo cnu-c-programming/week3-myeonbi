092b4f8257118a577c19c69c0831957b655ce74c
hyunbikim <152488051+myeonbi@users.noreply.github.com>
2026-03-20 11:52:26 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int add(int x, int y) {return x + y;};
+int sub(int x, int y) {return x - y;};
+int mul(int x, int y) {return x * y;};
+int div(int x, int y) {return x / y;};
 
+int main(void) {
+    int a = 10, b = 5;
+    printf("%d + %d = %d\n", a, b, add(a, b));
+    printf("%d - %d = %d\n", a, b, sub(a, b));
+    printf("%d * %d = %d\n", a, b, mul(a, b));
+    printf("%d / %d = %d\n", a, b, div(a, b));
     return 0;
 }
